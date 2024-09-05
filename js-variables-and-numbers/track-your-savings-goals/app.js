67f166f588fe333555892ad9b4bf4266e2809bd3
leonel-acosta <leoacosta88@gmail.com>
2024-09-05 11:39:01 +0200

    log calculation results

---
+++ js-variables-and-numbers/track-your-savings-goals/app.js
@@ -0,0 +1,7 @@
+const startingBalance = 1000;
+const monthlyContribution = 260;
+const months = 12;
+
+const totalSavings = startingBalance + (monthlyContribution * months)
+
+console.log("After " + months + "months of saving, you will have $"+ totalSavings+ " in your account.")
