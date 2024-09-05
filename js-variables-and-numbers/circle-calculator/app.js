969baa8489ebac55f7b6b4dccb0408d869ee88f0
leonel-acosta <leoacosta88@gmail.com>
2024-09-05 11:50:10 +0200

    add variables, calculations, and results logs

---
+++ js-variables-and-numbers/circle-calculator/app.js
@@ -0,0 +1,7 @@
+const radius = 5;
+const area = Math.PI * Math.pow(radius, 2)
+const circumference = 2 * Math.PI * radius
+
+console.log("For a circle with a radius of "+ radius +" units: " + area)
+console.log("The area is approximately " + area + " square units.")
+console.log("The circumference is approximately " + circumference)
