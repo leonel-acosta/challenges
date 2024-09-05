3c93af116f3a283b2e48021f22a540485c3e1b15
leonel-acosta <leoacosta88@gmail.com>
2024-09-05 11:30:32 +0200

    Add tip and meal costs variables and calculation logs

---
+++ js-variables-and-numbers/tip-calculator/app.js
@@ -0,0 +1,10 @@
+let mealCost = 64;
+let tipPercent = 10;
+
+const tipAmount = mealCost*tipPercent/100
+
+console.log("Tip Amount 10%:" + tipAmount + "EUR")
+
+const totalCost = mealCost + tipAmount;
+
+console.log ("The total cost of your bill is:" + totalCost + "EUR")
