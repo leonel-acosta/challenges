a65bfedf84015fed3910de4156e06359cd4927ba
leonel-acosta <leoacosta88@gmail.com>
2024-09-06 15:32:08 +0200

    add conditional logic to button

--- js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
@@ -9,4 +9,9 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the console.
+  if(age < 18){
+    console.log("You are a teen");
+  }
+  else {console.log("You are not a teen");}
 });
+
