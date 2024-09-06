7bf2c74b8f4144ae3e290f120e03e512efb77062
leonel-acosta <leoacosta88@gmail.com>
2024-09-06 11:52:43 +0200

    add functionality to buttons

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,26 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("Theme: Dark Mode")
+});
+
+lightModeButton.addEventListener("click", () => {
+    bodyElement.classList.remove("dark");
+    console.log("Theme: Light Mode")
+  });
+
+
+toggleButton.addEventListener("click", () => {
+    bodyElement.classList.toggle("dark");
+    console.log("Theme Toggle")
+  });
