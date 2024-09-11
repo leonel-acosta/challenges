bc3fb7d724a14c0beea4a05c53b9ec5de4e22369
leonel-acosta <leoacosta88@gmail.com>
2024-09-11 16:37:48 +0200

    submit functionality

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -8,6 +8,8 @@ function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
 
+hideTosError();
+
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
@@ -16,9 +18,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data)
+
+  if (event.target.tos.checked === false){
+    event.target.tos.focus;
+    showTosError()
+    console.log("TOS unchecked")
+    return; 
+  } 
+
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+alert("Form submitted");
 });
