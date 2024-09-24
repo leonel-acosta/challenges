1bc09231738304ea5d6bc6dbc8b4f9bd31c4bb27
leonel-acosta <leoacosta88@gmail.com>
2024-09-24 11:14:38 +0200

    fix error

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,8 +1,8 @@
 export function getAnimal(animals) {
 
     if (animals === "cats"){return 'I totally love cats!'}
-    if (animals === "dogs"){return 'I like dogs!'}
-    else {return 'I do not like animals at all!'} 
+    if (animals === undefined){return `I do not like animals at all!`}
+    else {return `I like ${animals}!`} 
 }
 
 getAnimal()
