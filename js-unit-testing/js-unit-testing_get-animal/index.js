a56f48ea1dcaf66903e4eb4b643e62001ec28dc6
leonel-acosta <leoacosta88@gmail.com>
2024-09-24 11:10:31 +0200

    Fix function in order to pass test

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+
+    if (animals === "cats"){return 'I totally love cats!'}
+    if (animals === "dogs"){return 'I like dogs!'}
+    else {return 'I do not like animals at all!'} 
+}
+
+getAnimal()
+getAnimal("dogs")
+getAnimal("cats")
+
