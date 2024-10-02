45ee5bfefdfd4fe7ab2f9b9de185bfb06079e6dc
leonel-acosta <leoacosta88@gmail.com>
2024-10-02 16:56:09 +0200

    Create Components for Journal App

--- react-setup/journal-app/src/App.jsx
+++ react-setup/journal-app/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Form from "./components/Form.jsx"
+import Input from "./components/Input.jsx"
+import Button from "./components/Button.jsx"
+import CommentsSection from "./components/CommentsSection.jsx"
+import Tabs from "./components/Tabs.jsx"
+import Comment from "./components/Comment.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Journal App</h1>
+      <Header />
+      <Form title="New Entry"> 
+        <Input label="Motto" id="motto" type="text" value="" name="Notes" />
+        <Input label="Notes" id="notes" type="textarea" value="" name="Notes" />
+        <Button text="Create" />
+      </Form>
+      <CommentsSection>
+        <Tabs summary="All entries">
+        </Tabs>
+        <Tabs summary="Favorite">
+        </Tabs>
+          <Comment date="FEB 27, 2024" title="That's life in the city" comment="A simple functional component that displays a comment with a title"/>
+          <Comment date="Oct 23, 2024" title="Not good" comment="A simple functional component that displays a comment with a title" favourite/>
+      </CommentsSection>
+      <Footer />
     </>
   )
 }
---
+++ react-setup/journal-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header>
+            <h1>Journal App</h1>
+        </header>
+    )
+}
