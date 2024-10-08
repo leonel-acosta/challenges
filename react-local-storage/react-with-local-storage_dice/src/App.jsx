3ee03b41e2e4e91be26103b769f78387ea8f8f14
leonel-acosta <leoacosta88@gmail.com>
2024-10-08 15:55:35 +0200

    lift up variables to parent element-refactory components

--- react-local-storage/react-with-local-storage_dice/src/App.jsx
+++ react-local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,15 +1,26 @@
-import { D6Button } from "./components/D6Button";
-import History from "./components/History";
+import useLocalStorageState from "use-local-storage-state"
 import "./App.css";
-
+import History from "./components/History";
+import { D6Button } from "./components/D6Button";
+import { getD6Roll } from "./utils";
+ 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls");
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  const currentRollValue = rolls[0]?.value;
+  console.log("Current Roll Value:", currentRollValue)
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll}/>
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
